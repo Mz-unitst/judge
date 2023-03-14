648380515bdb75529a736938192a3648c946176a
Mz-unitst <1764293088@qq.com>
2023-03-14 21:59:40 +0800

    first commit

---
+++ judge/src/web/t1.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "rm-bp1fhm86orq5947ns.mysql.rds.aliyuncs.com";
+$username = "zstu";
+$password = "Zstu1234";
+ 
+// 创建连接
+$conn = new mysqli($servername, $username, $password);
+ 
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+} 
+echo "连接成功";
+
+mysqli_close($conn);
+?>
